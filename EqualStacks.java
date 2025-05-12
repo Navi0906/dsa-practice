e99f15f4eff902a5c6cfcb4e70b9db2bb341c962
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-05-12 17:28:56 +0530

    Day-3 Problems

--- EqualStacks.java
+++ EqualStacks.java
@@ -1,72 +1,99 @@
+import java.io.*;
 import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class EqualStacks {
+class Result {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n1 = scanner.nextInt();
-        int n2 = scanner.nextInt();
-        int n3 = scanner.nextInt();
+    /*
+     * Complete the 'equalStacks' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h1
+     *  2. INTEGER_ARRAY h2
+     *  3. INTEGER_ARRAY h3
+     */
 
-        int[] h1 = new int[n1];
-        int[] h2 = new int[n2];
-        int[] h3 = new int[n3];
-
-        for (int i = 0; i < n1; i++) {
-            h1[i] = scanner.nextInt();
-        }
-
-        for (int i = 0; i < n2; i++) {
-            h2[i] = scanner.nextInt();
-        }
-
-        for (int i = 0; i < n3; i++) {
-            h3[i] = scanner.nextInt();
-        }
+    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
+        // Write your code here
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        Stack<Integer> stack3 = new Stack<>();
 
         int sum1 = 0, sum2 = 0, sum3 = 0;
 
-        Stack<Integer> s1 = new Stack<>();
-        Stack<Integer> s2 = new Stack<>();
-        Stack<Integer> s3 = new Stack<>();
-
-        for (int i = n1 - 1; i >= 0; i--) {
-            sum1 += h1[i];
-            s1.push(sum1);
+        for (int i = h1.size() - 1; i >= 0; i--) {
+            sum1 += h1.get(i);
+            stack1.push(sum1);
         }
-
-        for (int i = n2 - 1; i >= 0; i--) {
-            sum2 += h2[i];
-            s2.push(sum2);
+        for (int i = h2.size() - 1; i >= 0; i--) {
+            sum2 += h2.get(i);
+            stack2.push(sum2);
         }
-
-        for (int i = n3 - 1; i >= 0; i--) {
-            sum3 += h3[i];
-            s3.push(sum3);
+        for (int i = h3.size() - 1; i >= 0; i--) {
+            sum3 += h3.get(i);
+            stack3.push(sum3);
         }
 
         while (true) {
-            if (s1.isEmpty() || s2.isEmpty() || s3.isEmpty()) {
-                System.out.println(0);
-                return;
+            if (stack1.isEmpty() || stack2.isEmpty() || stack3.isEmpty()) {
+                return 0;
             }
 
-            int top1 = s1.peek();
-            int top2 = s2.peek();
-            int top3 = s3.peek();
+            int height1 = stack1.peek();
+            int height2 = stack2.peek();
+            int height3 = stack3.peek();
 
-            if (top1 == top2 && top2 == top3) {
-                System.out.println(top1);
-                return;
+            if (height1 == height2 && height2 == height3) {
+                return height1;
             }
 
-            if (top1 >= top2 && top1 >= top3) {
-                s1.pop();
-            } else if (top2 >= top1 && top2 >= top3) {
-                s2.pop();
+
+            if (height1 >= height2 && height1 >= height3) {
+                stack1.pop();
+            } else if (height2 >= height1 && height2 >= height3) {
+                stack2.pop();
             } else {
-                s3.pop();
+                stack3.pop();
             }
         }
+    }
+
+}
+
+public class EqualStacks {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int n2 = Integer.parseInt(firstMultipleInput[1]);
+
+        int n3 = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> h1 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> h2 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> h3 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.equalStacks(h1, h2, h3);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
